8bc289e725099b60a4e6719fec62aed08f414479
unknown <davidchinski@gmail.com>
2025-05-23 21:08:06 -0300

    terminar navegaciones

--- TP/App.js
+++ TP/App.js
@@ -1,20 +1,24 @@
-import * as React from 'react';
-import { Button, TextInput, Text, View, StyleSheet, TouchableOpacity } from 'react-native';
-import { NavigationContainer, useNavigation } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import * as React from "react";
+import {
+  TextInput,
+  Text,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
-import { Ionicons } from '@expo/vector-icons';
-
-//
-// Screens del Primer Stack
-//
 function ScreenA1() {
   const navigation = useNavigation();
   return (
     <View style={styles.homeScreen}>
-      <Text style={styles.text}>HOME</Text>
-      ...
+      <Text style={styles.text}>esta es la HOME</Text>
+      <TouchableOpacity onPress={() => navigation.navigate("ScreenA2")}>
+        <Text style={styles.text}>Ir a HOME 2</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -24,25 +28,67 @@ function ScreenA2() {
   return (
     <View style={styles.homeScreen}>
       <Text style={styles.text}>HOME 2</Text>
-      ...
     </View>
   );
 }
 
-// Screens del Primer Stack
 function ScreenB1() {
-
+  const navigation = useNavigation();
+  return (
+    <View style={styles.homeScreen}>
+      <Text style={styles.text}>esta es el BUSCADOR</Text>
+      <TouchableOpacity onPress={() => navigation.navigate("ScreenB2")}>
+        <Text style={styles.text}>Ir a BUSCADOR 2</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
 function ScreenB2() {
-
+  const navigation = useNavigation();
+  return (
+    <View style={styles.homeScreen}>
+      <Text style={styles.text}>BUSCADOR 2</Text>
+    </View>
+  );
 }
 
-// Screens del Tercer Stack
 function ScreenC1() {
-
+  const navigation = useNavigation();
+  return (
+    <View style={styles.homeScreen}>
+      <Text style={styles.text}>esta es el PERFIL</Text>
+      <TouchableOpacity onPress={() => navigation.navigate("ScreenC2")}>
+        <Text style={styles.text}>Ir a PERFIL 2</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
 function ScreenC2() {
-
+  const navigation = useNavigation();
+  return (
+    <View style={styles.homeScreen}>
+      <Text style={styles.text}>PERFIL 2</Text>
+    </View>
+  );
+}
+function ScreenD1() {
+  const navigation = useNavigation();
+  return (
+    <View style={styles.homeScreen}>
+      <Text style={styles.text}>esta es la CONFIGURACION</Text>
+      <TouchableOpacity onPress={() => navigation.navigate("ScreenD2")}>
+        <Text style={styles.text}>Ir a CONFIGURACION 2</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+function ScreenD2() {
+  const navigation = useNavigation();
+  return (
+    <View style={styles.homeScreen}>
+      <Text style={styles.text}>CONFIGURACION 2</Text>
+    </View>
+  );
 }
 
 //
@@ -51,7 +97,8 @@ function ScreenC2() {
 const StackA = createNativeStackNavigator();
 const StackB = createNativeStackNavigator();
 const StackC = createNativeStackNavigator();
- 
+const StackD = createNativeStackNavigator();
+
 function StackANavigator() {
   return (
     <StackA.Navigator>
@@ -62,24 +109,63 @@ function StackANavigator() {
 }
 
 function StackBNavigator() {
-
+  return (
+    <StackB.Navigator>
+      <StackB.Screen name="ScreenB1" component={ScreenB1} />
+      <StackB.Screen name="ScreenB2" component={ScreenB2} />
+    </StackB.Navigator>
+  );
 }
-function StackCNavigator() {}
+function StackCNavigator() {
+  return (
+    <StackC.Navigator>
+      <StackC.Screen name="ScreenC1" component={ScreenC1} />
+      <StackC.Screen name="ScreenC2" component={ScreenC2} />
+    </StackC.Navigator>
+  );
+}
 
-//
-// Creación del BottomTabNavigator
-//
+function StackDNavigator() {
+  return (
+    <StackD.Navigator>
+      <StackD.Screen name="ScreenD1" component={ScreenD1} />
+      <StackD.Screen name="ScreenD2" component={ScreenD2} />
+    </StackD.Navigator>
+  );
+}
+
 const Tab = createBottomTabNavigator();
 function MyTabs() {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Home"     component={StackANavigator} />
-      <Tab.Screen name="Buscador" component={StackBNavigator} />
-      <Tab.Screen name="Perfil"   component={StackCNavigator}
+      <Tab.Screen
+        name="Home"
+        component={StackANavigator}
         options={{
-          tabBarIcon: ({ color }) => (
-            <Ionicons name="person" size={24} color={color} />
-           ),
+          tabBarIcon: () => <Ionicons name="home" size={24} color="black" />,
+        }}
+      />
+      <Tab.Screen
+        name="Buscador"
+        component={StackBNavigator}
+        options={{
+          tabBarIcon: () => <Ionicons name="search" size={24} color="black" />,
+        }}
+      />
+      <Tab.Screen
+        name="Perfil"
+        component={StackCNavigator}
+        options={{
+          tabBarIcon: () => <Ionicons name="person" size={24} color="black" />,
+        }}
+      />
+      <Tab.Screen
+        name="Configuración"
+        component={StackDNavigator}
+        options={{
+          tabBarIcon: () => (
+            <Ionicons name="settings" size={24} color="black" />
+          ),
         }}
       />
     </Tab.Navigator>
@@ -98,18 +184,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'black', 
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "black",
   },
   text: {
-    color: 'white',
+    color: "black",
+    padding: 10,
     fontSize: 20,
   },
   homeScreen: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor:'#ff0000'
-  }
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "gray",
+    
+  },
 });
